2bd69660db3c68a89f15fe23fd79b8509aa570ba
Borys Romanyuk <borysr@gmail.com>
2025-08-04 08:58:51 -0500

    initial creation . MoreZeroes excersize

--- ClassLibrary/obj/Debug/net9.0/ClassLibrary.AssemblyInfo.cs
+++ ClassLibrary/obj/Debug/net9.0/ClassLibrary.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efb737fc041bcc81b015b6471cfec912083f077a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClassLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
