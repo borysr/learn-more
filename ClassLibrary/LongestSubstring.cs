32e89fb84405966903491b499e8657f100aa0113
Borys Romanyuk <bromanyuk@s3gov.com>
2025-08-08 13:33:11 -0500

    Longest substring length excersize added

---
+++ ClassLibrary/LongestSubstring.cs
@@ -0,0 +1,25 @@
+using System.Reflection;
+
+public class LongestSubstring
+{
+
+    public int LengthOfLongestSubstring(string s)
+    {
+        int l = 0;
+        int r = 0;
+        int res = 0;
+        var map = new Dictionary<char, int>();
+
+        for (int i = 0; i < s.Length; ++i)
+        {
+            if (map.ContainsKey(s[i]))
+            {
+                l = Math.Max(l, map[s[i]]+1);
+            }
+            r++;
+            map[s[i]] = i;
+            res = Math.Max(res, r - l);
+        }
+        return res;
+    }
+}
---
+++ UnitTests/LongestSubstringTests.cs
@@ -0,0 +1,23 @@
+namespace UnitTests;
+
+public class LongestSubstringTests
+{
+    [Theory]
+    [InlineData("", 0)]
+    [InlineData("aaaaa", 1)]
+    [InlineData("abcaabcdaeaa", 5)]
+    [InlineData("abcaabcabcbcd", 3)]
+    [InlineData("abcabcbb", 3)]
+    [InlineData("pwwkew", 3)]
+    public void Test1(string input, int expected)
+    {
+        // Arrange
+        var sut = new LongestSubstring();
+
+        // Act
+        var actual = sut.LengthOfLongestSubstring(input);
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+}
