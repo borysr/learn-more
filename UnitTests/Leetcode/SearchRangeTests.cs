f735862635a83177f371873903d3d31574bc8923
Borys Romanyuk <borysr@gmail.com>
2025-11-04 17:28:22 +0000

    refactor: Clean up InlineData formatting in SearchRangeTests

--- UnitTests/Leetcode/SearchRangeTests.cs
+++ UnitTests/Leetcode/SearchRangeTests.cs
@@ -3,9 +3,12 @@ namespace UnitTests
     public class SearchRangeTests
     {
         [Theory]
-//        [InlineData(new int[] { }, 0, new int[] { -1, -1 })]
+        [InlineData(new int[] { }, 0, new int[] { -1, -1 })]
         [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 4, new int[] { 4, 4 })]
- //       [InlineData(new int[] { 0, 1, 4, 4, 4, 5 }, 4, new int[] { 2, 4 })]
+        [InlineData(new int[] { 0, 1, 4, 4, 4, 5 }, 4, new int[] { 2, 4 })]
+        [InlineData(new int[] { 10, 11, 11, 11, 14, 15 }, 11, new int[] { 1, 3 })]
+        [InlineData(new int[] { 10, 11, 11, 11, 14, 15 }, 10, new int[] { 0, 0 })]
+        [InlineData(new int[] { 10, 11, 11, 11, 14, 15 }, 15, new int[] { 5, 5 })]
         public void TestV2(int[] arr, int item, int[] expected)
         {
             var sut = new LeetcodeBase { };
@@ -14,9 +17,12 @@ namespace UnitTests
         }
         
         [Theory]
-        [InlineData(new int[] { }, 0, new int[] { -1, -1})]
-        [InlineData(new int[] {0,1,2,3,4,5 }, 4, new int[] { 4, 4})]
-        [InlineData(new int[] {0,1,4,4,4,5 }, 4, new int[] { 2, 4})]
+        [InlineData(new int[] { }, 0, new int[] { -1, -1 })]
+        [InlineData(new int[] { 0, 1, 2, 3, 4, 5 }, 4, new int[] { 4, 4 })]
+        [InlineData(new int[] { 0, 1, 4, 4, 4, 5 }, 4, new int[] { 2, 4 })]
+        [InlineData(new int[] { 10, 11, 11, 11, 14, 15 }, 11, new int[] { 1, 3 })]
+        [InlineData(new int[] { 10, 11, 11, 11, 14, 15 }, 10, new int[] { 0, 0 })]
+        [InlineData(new int[] { 10, 11, 11, 11, 14, 15 }, 15, new int[] { 5, 5 })]
         public void TestV1(int[] arr, int item,  int[] expected)
         {
             var sut = new LeetcodeBase { };
