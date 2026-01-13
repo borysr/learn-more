ffc2439e3957fad337d4a5dc94d3e69548912674
Borys Romanyuk <borysr@gmail.com>
2026-01-13 10:42:18 -0600

    feat: Add initial implementation of CA_ParallelLoopsExceptions project with parallel sum calculation and exception handling

---
+++ CA_ParallelLoopsExceptions/Program.cs
@@ -0,0 +1,34 @@
+﻿int[] array = Enumerable.Range(0, 100).ToArray();
+
+int sum = 0;
+object lockSum = new object();
+
+
+try
+{
+    Parallel.For(0, array.Length, (i, state) =>
+    {
+        lock (lockSum)
+        {
+            if (!state.IsExceptional) // Check if an exception has already occurred on any thread  
+            {
+                if (i == 65)
+                    throw new InvalidOperationException("This is on purpose.");
+
+                sum += array[i];
+                Console.WriteLine($"Item: {i} Current task id: {Task.CurrentId}; Is thread pool thread: {Thread.CurrentThread.IsThreadPoolThread}");
+            }
+        }
+    });
+
+   
+}
+catch(AggregateException ex)
+{
+    Console.WriteLine(ex);
+}
+
+Console.WriteLine($"The sum is {sum}");
+
+Console.ReadLine();
+
